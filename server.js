8a08ef7de477672ec56774229a852afd1f748dcb
XaLiLiY <125592271+XaLiLiY@users.noreply.github.com>
2026-02-07 10:19:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -394,6 +394,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
